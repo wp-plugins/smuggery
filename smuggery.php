bed7ea7a41e9e530b99e1162c1eeca8bd1cf64d0
freerobby <freerobby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-16 20:55:54 +0000

    Initial import (just placeholder header)
    git-svn-id: https://plugins.svn.wordpress.org/smuggery/trunk@74276 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ smuggery.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Smuggery
+
+Plugin URI: http://www.freerobby.com/smuggery
+
+Description: Smuggery pulls all of your public photos from your SmugMug.com account and generates dynamic Wordpress photo galleries from them to be displayed on your blog!
+
+Author: Robby Grossman
+
+Version: 1.0
+
+Author URI: http://www.freerobby.com
+*/	
+?>
