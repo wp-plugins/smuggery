b8e82d19f89c57e08500fc929b2728787179d831
freerobby <freerobby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-18 22:43:20 +0000

    Placeholder plugin now installs and options page demonstrates functionality of updating options to database.
    git-svn-id: https://plugins.svn.wordpress.org/smuggery/trunk@74678 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smuggery-options.php
+++ smuggery-options.php
@@ -5,3 +5,51 @@ file:	smuggery-options.php
 desc:	Options page for Smuggery Wordpress Plugin	
 */
 ?>
+<?php
+// If the form was just submitted (options updated), ...
+if ( isset ( $_POST [ 'submitted' ] ) ) {
+	update_option ( 'smuggery_galleryTitle', htmlspecialchars ( $_POST [ 'smuggery_galleryTitle' ], ENT_QUOTES ) );
+	update_option ( 'smuggery_smugmugUsername', htmlspecialchars ( $_POST [ 'smuggery_smugmugUsername' ], ENT_QUOTES ) );
+?>
+	<div id="message" class="updated fade">
+		<p>
+			<strong>
+				Options saved!
+			</strong>
+		</p>
+	</div>
+<?php
+}
+?>
+
+<div class="wrap">
+<form action="<?php echo $_SERVER ['REQUEST_URI']; ?>" method="post">
+
+<h2>
+	Smuggery Options
+</h2>
+
+<h3>
+	Display Options
+</h3>
+
+<p>
+	Title of gallery:
+	<br />
+	<input type="text" name="smuggery_galleryTitle" size="100" value="<?php echo stripslashes ( get_option ( 'smuggery_galleryTitle' ) ); ?>" />
+</p>
+
+<h3>
+	SmugMug Account Details
+</h3>
+<p>
+	SmugMug Username: 
+	<input type="text" name="smuggery_smugmugUsername" maxlength="100" size="100" value="<?php echo stripslashes ( get_option ( 'smuggery_smugmugUsername' ) ) ?>" />
+</p>
+
+<input type="submit" name="submitted" value="Update Options &raquo;" />
+
+<!-- End of Options Page -->
+	</form>
+</div>
+<!-- End of End of Options Page -->
--- smuggery.php
+++ smuggery.php
@@ -8,8 +8,83 @@ Description: Smuggery pulls all of your public photos from your SmugMug.com acco
 
 Author: Robby Grossman
 
-Version: 1.0
+Version: 0.1
 
 Author URI: http://www.freerobby.com
 */	
+?>
+<?php
+////////////////////
+// Plugin options //
+////////////////////
+// Option keys
+global $ds_ap_options_names;
+$ds_ap_options_names = array (
+	'smuggery_galleryTitle',
+	'smuggery_smugmugUsername'
+	);
+
+global $ds_ap_options_vals;
+$ds_ap_options_vals = array (
+	"My Gallery",
+	NULL
+);
+
+//////////////
+// Includes //
+//////////////
+
+// Wordpress Administrative Functions
+require_once ( ABSPATH . '/wp-admin/admin-functions.php' );
+
+/////////////////////////////////
+// Wordpress Hook Declarations //
+/////////////////////////////////
+
+// Plugin activation
+register_activation_hook ( __FILE__ , 'smuggery_install' );
+// Plugin deactivation
+register_deactivation_hook ( __FILE__ , 'smuggery_uninstall' );
+// Add our page to the administration menu
+add_action('admin_menu', 'smuggery_addpages');
+// Before content is displayed, let us look through it and make changes as necessary.
+add_filter ('the_content', 'smuggery_parsecontent');
+
+////////////////////////////////
+// Wordpress Hook Invocations //
+////////////////////////////////
+// Define our configuration pages
+function smuggery_addpages () {
+	// Add our menu under "options"
+	add_options_page ( 'Smuggery', 'Smuggery', 'edit_plugins', __FILE__, 'smuggery_optionspage');
+}
+// Content interrupt entry point!
+function smuggery_parsecontent ( $content ) {
+	return $content;
+}
+function smuggery_install () {
+	// Add our options with their default values as defined.
+	global $ds_ap_options_names;
+	global $ds_ap_options_vals;
+	$num_opts = count($ds_ap_options_names);
+	for ($i = 0; $i < $num_opts; $i++) {
+		add_option($ds_ap_options_names[$i], $ds_ap_options_vals[$i]);
+	}
+}
+function smuggery_uninstall () {
+	// Remove options from database on uninstall.
+	//global $ds_ap_options_names;
+	//$num_opts = count($ds_ap_options_names);
+	//for ($i = 0; $i < $num_opts; $i++) {
+	//	delete_option($ds_ap_options_names[$i], $ds_ap_options_vals[$i]);
+	//}
+}
+
+///////////////////
+// Options pages //
+///////////////////
+// Define our options page
+function smuggery_optionspage () {
+	include  ( 'smuggery-options.php' );
+}
 ?>
